8160443f68c8b6f09cd8678a2acaa8396645d05a
Berkan Yavri <berkan.yavri@deliveryhero.com>
2019-07-20 22:31:42 +0200

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
-export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
+if [ $1 == "ubuntu" ]; then
+  apt install openssl
+  export OPENSSL_INCLUDE_DIR=`which $(openssl)`/include
+  export OPENSSL_LIB_DIR=`which $(openssl)`/lib
+elif [ $1 == "redhat" ]; then
+  yum install openssl
+  export OPENSSL_INCLUDE_DIR=`which $(openssl)`/include
+  export OPENSSL_LIB_DIR=`which $(openssl)`/lib
+else
+  export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
+  export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
+fi
 
 cargo run
